fba07ec27eb788662f66f0a2a38912c0fb32e785
Dkui <msksnkx@163.com>
2024-03-21 09:52:56 +0800

    fix：结构调整

---
+++ gulimall-coupon/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Author Dkui
+ * @Date 2024/3/21 9:48
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
