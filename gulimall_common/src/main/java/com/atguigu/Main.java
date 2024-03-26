80af788ee78488171e19fc45c721ff46fc7f49ce
Dkui <44127730+Dkui97@users.noreply.github.com>
2024-03-26 23:38:23 +0800

    fix：初始化

---
+++ gulimall_common/src/main/java/com/atguigu/Main.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @Author Dkui
+ * @Date 2024/3/21 16:23
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
