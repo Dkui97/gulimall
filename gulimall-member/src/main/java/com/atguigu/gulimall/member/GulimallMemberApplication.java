a15392a5d164fe87e176efccbdc451eb60b824f1
Dkui <msksnkx@163.com>
2024-04-02 11:25:44 +0800

    feat：新增openfeign

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -3,7 +3,15 @@ package com.atguigu.gulimall.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * feign调用
+ * 1.引入依赖
+ *
+ *
+ */
+@EnableFeignClients(basePackages="com.atguigu.gulimall.member.feign")//扫描接口方法注解
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallMemberApplication {
