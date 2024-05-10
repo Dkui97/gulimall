148c696ab43071bc155190a990e8963a333b13c7
Dkui <msksnkx@163.com>
2024-05-10 16:30:41 +0800

    feat:登录跨域处理

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package com.atguigu.gulimall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @Author Dkui
+ * @Date 2024/5/10 16:16
+ * @Description:
+ */
+
+@Configuration // 配置类
+public class CorsConfig {
+
+    @Bean // 添加过滤器
+    public CorsWebFilter corsWebFilter(){
+        // 基于url跨域，选择reactive包下的
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        // 跨域配置信息
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 允许跨域的头
+        corsConfiguration.addAllowedHeader("*");
+        // 允许跨域的请求方式
+        corsConfiguration.addAllowedMethod("*");
+        // 允许跨域的请求来源
+        corsConfiguration.addAllowedOriginPattern("*");
+        // 是否允许携带cookie跨域
+        corsConfiguration.setAllowCredentials(true);
+
+        // 任意url都要进行跨域配置
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
